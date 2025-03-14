d8b3d20878944890a7a655ebe86bc65fda0b5fc2
Morten Scheel <morten@mortenscheel.com>
2025-03-14 21:19:01 +0100

    feat: Initial implementation

---
+++ src/functions.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Scheel\EditorLinks;
+
+use function array_key_exists;
+use function count;
+use function getenv;
+use function is_string;
+
+function editorLink(string $file, int|string $line): string
+{
+    $editors = [
+        'sublime' => 'subl://open?url=file://%file&line=%line',
+        'textmate' => 'txmt://open?url=file://%file&line=%line',
+        'emacs' => 'emacs://open?url=file://%file&line=%line',
+        'macvim' => 'mvim://open/?url=file://%file&line=%line',
+        'phpstorm' => 'phpstorm://open?file=%file&line=%line',
+        'idea' => 'idea://open?file=%file&line=%line',
+        'vscode' => 'vscode://file/%file:%line',
+        'vscode-insiders' => 'vscode-insiders://file/%file:%line',
+        'vscode-remote' => 'vscode://vscode-remote/%file:%line',
+        'vscode-insiders-remote' => 'vscode-insiders://vscode-remote/%file:%line',
+        'vscodium' => 'vscodium://file/%file:%line',
+        'nova' => 'nova://core/open/file?filename=%file&line=%line',
+        'xdebug' => 'xdebug://%file@%line',
+        'atom' => 'atom://core/open/file?filename=%file&line=%line',
+        'espresso' => 'x-espresso://open?filepath=%file&lines=%line',
+        'netbeans' => 'netbeans://open/?f=%file:%line',
+    ];
+    $editor = getenv('EDITOR_LINK_EDITOR');
+    if (! is_string($editor) || ! array_key_exists($editor, $editors)) {
+        $editor = 'phpstorm';
+    }
+    $format = $editors[$editor];
+    if ($mapping = getenv('EDITOR_LINK_MAPPING')) {
+        $paths = explode(':', $mapping);
+        if (count($paths) === 2 && $paths[0] !== '' && $paths[1] !== '') {
+            $file = str_replace($paths[0], $paths[1], $file);
+        }
+    }
+
+    return str_replace(['%file', '%line'], [$file, (string) $line], $format);
+}
---
+++ tests/EditorLinksTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+use function Scheel\EditorLinks\editorLink;
+
+arch()->preset()->php();
+arch()->preset()->security();
+
+beforeEach(function (): void {
+    putenv('EDITOR_LINK_EDITOR=');
+    putenv('EDITOR_LINK_MAPPING=');
+});
+
+it('works without environment variables', function (): void {
+    expect(editorLink('/project/file.php', 10))
+        ->toBe('phpstorm://open?file=/project/file.php&line=10');
+});
+
+it('can use valid path mapping', function (): void {
+    putenv('EDITOR_LINK_MAPPING=/project:/mapped');
+    expect(editorLink('/project/file.php', 10))
+        ->toBe('phpstorm://open?file=/mapped/file.php&line=10');
+});
+
+it('ignores invalid path mapping', function (): void {
+    putenv('EDITOR_LINK_MAPPING=1:2:3');
+    expect(editorLink('/project/file.php', 10))
+        ->toBe('phpstorm://open?file=/project/file.php&line=10');
+});
+
+it('uses fallback if editor is invalid', function (): void {
+    putenv('EDITOR_LINK_EDITOR=invalid');
+    expect(editorLink('/project/file.php', 10))
+        ->toBe('phpstorm://open?file=/project/file.php&line=10');
+});
+
+it('can use valid editor', function (string $editor, string $expected): void {
+    putenv("EDITOR_LINK_EDITOR=$editor");
+    expect(editorLink('/project/file.php', 10))
+        ->toBe($expected);
+})->with([
+    'sublime' => ['sublime', 'subl://open?url=file:///project/file.php&line=10'],
+    'textmate' => ['textmate', 'txmt://open?url=file:///project/file.php&line=10'],
+    'emacs' => ['emacs', 'emacs://open?url=file:///project/file.php&line=10'],
+    'macvim' => ['macvim', 'mvim://open/?url=file:///project/file.php&line=10'],
+    'phpstorm' => ['phpstorm', 'phpstorm://open?file=/project/file.php&line=10'],
+    'idea' => ['idea', 'idea://open?file=/project/file.php&line=10'],
+    'vscode' => ['vscode', 'vscode://file//project/file.php:10'],
+    'vscode-insiders' => ['vscode-insiders', 'vscode-insiders://file//project/file.php:10'],
+    'vscode-remote' => ['vscode-remote', 'vscode://vscode-remote//project/file.php:10'],
+    'vscode-insiders-remote' => ['vscode-insiders-remote', 'vscode-insiders://vscode-remote//project/file.php:10'],
+    'vscodium' => ['vscodium', 'vscodium://file//project/file.php:10'],
+    'nova' => ['nova', 'nova://core/open/file?filename=/project/file.php&line=10'],
+    'xdebug' => ['xdebug', 'xdebug:///project/file.php@10'],
+    'atom' => ['atom', 'atom://core/open/file?filename=/project/file.php&line=10'],
+    'espresso' => ['espresso', 'x-espresso://open?filepath=/project/file.php&lines=10'],
+    'netbeans' => ['netbeans', 'netbeans://open/?f=/project/file.php:10'],
+]);
